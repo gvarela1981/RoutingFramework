cefff00506fbc32cce13381eb9b02bffa49b2a5c
Gonzalo Varela <gonzalo.varela1981@gmail.com>
2020-04-21 19:34:45 -0300

    Se restaura conexion a postgis perdida
    En algun merge se perdio conexion a la DB, se vuelve a configurar y se prueba tanto la edicion de los modelos desde django admin como las respuestas de la API para destino dentro de CABA como para destino fuera de CABA

--- totem_taxi/settings.py
+++ totem_taxi/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'totem_taxi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'api_taxi',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '192.168.0.9',
+        'PORT': '5444',
     }
 }
 
